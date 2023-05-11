0030e97a78df883d295e889b0e2f1b99473b0991
unknown <sudsier.gabriel@gmail.com>
2023-05-10 22:14:26 -0300

    Changed imgs (still without backend)

--- mgm-digital-drone/src/sections/Portfolio.tsx
+++ mgm-digital-drone/src/sections/Portfolio.tsx
@@ -1,10 +1,10 @@
-const imgList = [
-    'https://cdn.britannica.com/54/101754-050-3FA9B4A0/Downtown-Sao-Paulo.jpg',
-    'https://s4.static.brasilescola.uol.com.br/be/2022/05/floresta-amazonica-rio-amazonas.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/d/dc/Serra_P4.JPG',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Praca_do_Papa%2C_Belo_Horizonte.jpg/640px-Praca_do_Papa%2C_Belo_Horizonte.jpg',
-    'https://i0.statig.com.br/bancodeimagens/4z/fi/hm/4zfihmb25m6hi9hkf2fh47m60.jpg'
-]
+import img01 from '../assets/portfolio/01.jpg'
+import img02 from '../assets/portfolio/02.jpg'
+import img03 from '../assets/portfolio/03.jpg'
+import img04 from '../assets/portfolio/04.jpg'
+import img05 from '../assets/portfolio/05.png'
+
+const images = [img01, img02, img03, img04, img05]
 
 const Item = (props: { img: string }) => (
     <div style={{ backgroundImage: `url(${props.img})` }} className="bg-cover bg-center h-[300px] w-full hover:scale-x-110 transition-all">
@@ -17,8 +17,8 @@ export const Portfolio = () => {
         <section id="portfolio">
             <div className="text-center mt-10">
                 <h1>Portfolio</h1>
-                <ul className="grid grid-cols-1 md:grid-cols-5 p-4 pr-8">
-                    {imgList.map(img => (
+                <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 p-4 pr-8">
+                    {images.map(img => (
                         <Item img={img} key={img} />
                     ))}
                 </ul>
--- mgm-digital-drone/src/sections/RankedWorks.tsx
+++ mgm-digital-drone/src/sections/RankedWorks.tsx
@@ -1,19 +1,19 @@
-import Img1 from '../assets/ilhabela/ilhabela1.jpg'
-import Img2 from '../assets/ilhabela/ilhabela2.jpg'
+import Img1 from '../assets/ilhabela/ilhabela2.jpg'
+import Img2 from '../assets/rankedworks/01.jpg'
 
 export const RankedWorks = () => {
     return (
         <section id="ranked-works" className="mt-4 px-4">
             <ul className="flex flex-wrap gap-4 pr-4">
                 <li className="relative flex-1 opacity-80 hover:opacity-100 transition-all min-w-[250px]">
-                    <img src={Img2} alt="" className="h-full" />
-
-                    <h2 className="absolute bottom-4 left-4 font-bold">Ilha Bela</h2>
-                </li>
-                <li className="relative flex-1 opacity-80 hover:opacity-100 transition-all min-w-[250px]">
                     <img src={Img1} alt="" className="h-full" />
 
-                    <h2 className="absolute bottom-4 left-4 font-bold">Ilha Bela</h2>
+                    <h2 className="absolute bottom-4 left-4 font-bold shadow-filter shadow-full-opacity">Ilha Bela</h2>
+                </li>
+                <li className="relative flex-1 opacity-80 hover:opacity-100 transition-all min-w-[250px]">
+                    <img src={Img2} alt="" className="h-full" />
+
+                    <h2 className="absolute bottom-4 left-4 font-bold shadow-filter shadow-full-opacity">Iluminação Pindamonhangaba</h2>
                 </li>
             </ul>
         </section>
